13719cbdba004e2dc4b2dc5aa64ef32ee1219491
mhtbhavsar <mhtbhavsar@gmail.com>
2025-05-20 18:15:21 +0530

    creating youtube like front end app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+// import { useState } from 'react';
+// import VideoPlayer from './components/VideoPlayer';
 
-import { useState } from 'react';
-import VideoPlayer from './components/VideoPlayer';
+const Container = styled.div`
+  display: flex
+`
+const Main = styled.div``;
+
+const Wrapper = styled.div``;
 
 function App() {
 
-  const [videoId, setVideoId] = useState(null)
+  /** Video player code commented */
+  // const [videoId, setVideoId] = useState(null)
 
-  function playVideo(e, videoId){
-    e.preventDefault()
-    setVideoId(videoId)
-  }
+  // function playVideo(e, videoId){
+  //   e.preventDefault()
+  //   setVideoId(videoId)
+  // }
+
+  // return (
+  //   <div className="App">
+  //     {videoId && <VideoPlayer videoId={videoId}></VideoPlayer>} <br />
+  //     <button onClick={(e)=>{playVideo(e, 'f1')}}>Play Video 1</button>
+  //     <button onClick={(e)=>{playVideo(e, 'f2')}}>Play Video 2</button>
+  //     <button onClick={(e)=>{playVideo(e, 'f3')}}>Play Video 3</button>
+  //   </div>
+  // );
 
   return (
-    <div className="App">
-      {videoId && <VideoPlayer videoId={videoId}></VideoPlayer>} <br />
-      <button onClick={(e)=>{playVideo(e, 'f1')}}>Play Video 1</button>
-      <button onClick={(e)=>{playVideo(e, 'f2')}}>Play Video 2</button>
-      <button onClick={(e)=>{playVideo(e, 'f3')}}>Play Video 3</button>
-    </div>
-  );
+    <Container>
+      <Menu/>
+      <Main>
+        <Navbar/>
+        <Wrapper>
+          Video cards
+        </Wrapper>
+      </Main>
+    </Container>
+  )
 }
 
 export default App;
